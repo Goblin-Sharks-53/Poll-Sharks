710614504838e390950178d01a7b1a56b0c1939a
Brandon <bbeuttel@gmail.com>
2025-03-06 13:54:13 -0800

    CSS Overhaul, Cosmo Icon added, changed Website header

--- client/src/pages/NavBar.jsx
+++ client/src/pages/NavBar.jsx
@@ -28,26 +28,26 @@ const NavBar = () => {
   
   return (
     <div className='navBar'>
-       <span
+       <span class='highlight' 
         onClick={() =>
             navigate('/dashboard', { state: { username: `${username}` } })} 
-        style={{ cursor: 'pointer', margin: '0 15px', color: 'blue' }}
+        style={{ cursor: 'pointer', margin: '0 15px', color: 'white' }}
       >
         HOME
       </span>
-      <span
+      <span class='highlight'
         onClick={viewPastPollsHandleClick}
-        style={{ cursor: 'pointer', margin: '0 15px', color: 'blue' }}
+        style={{ cursor: 'pointer', margin: '0 15px', color: 'white' }}
       >
         PAST POLLS
       </span>
-      <span
+      <span class='highlight'
         onClick={newPollHandleClick}
-        style={{ cursor: 'pointer', margin: '0 15px', color: 'blue' }}
+        style={{ cursor: 'pointer', margin: '0 15px', color: 'white' }}
       >
         CREATE POLLS
       </span>
-      <button onClick={handleClickLogout}>Logout</button>
+      <button className='logout' onClick={handleClickLogout}>Logout</button>
     </div>
   );
 };
